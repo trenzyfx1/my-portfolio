8f55b0b00073702e1cbd5e5a68fbdebda874aee5
trenzyfx1 <rolemodel070@gmail.com>
2025-03-20 12:57:20 +0000

    milestone changes

--- JS Minifier/contact.js
+++ JS Minifier/contact.js
@@ -8,17 +8,4 @@ document.addEventListener("DOMContentLoaded", () => {
         element.classList.add('fade-in');
     });
 
-    const contactForm = document.getElementById('contact-form');
-    contactForm.addEventListener('submit', (event) => {
-        event.preventDefault();
-
-        const name = document.getElementById('Christian Treasure').value;
-        const email = document.getElementById('Email Address').value;
-        const message = document.getElementById('Your Message Here').value;
-
-        console.log(`Name: ${name}, Email: ${email}, Message: ${message}`);
-
-        contactForm.reset();
-        alert('Thank you for your message! I will get back to you soon.');
-    });
 });
